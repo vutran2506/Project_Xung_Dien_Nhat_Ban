0bfbb0267d39bb720accabf058fc4adc5278709c
vutran2506 <tranngocvu1832@gmail.com>
2023-04-17 10:57:47 +0700

    spring

--- database.sql
+++ database.sql
@@ -1,17 +1,18 @@
-create database cua_hang_xung_dien_nhat_ban;
-use cua_hang_xung_dien_nhat_ban;
+
+ create database slg_corporation_japan;
+use slg_corporation_japan;
 create table role (
 id int primary key auto_increment,
 name varchar(25)
 );
 create table account(
-id int primary key auto_increment,
+id bigint primary key auto_increment,
 password varchar(225)
 );
 create table account_role (
-id int  primary key auto_increment,
+id bigint  primary key auto_increment,
 id_role int,
-id_account int ,
+id_account bigint ,
 foreign key (id_role) references `role`(id),
 foreign key (id_account) references account(id)
 );
@@ -20,58 +21,65 @@ id int primary key auto_increment,
 name varchar(45)
 );
 create	table employee(
-id int  primary key auto_increment,
+id bigint  primary key auto_increment,
 name varchar(45),
 phone_number varchar(45),
 id_card varchar(45) unique,
 address varchar (225),
 gender boolean,
-is_deleted boolean,
 day_of_birth date,
 position_id int,
 email_account varchar(45) unicode,
-id_account int,
+is_deleted boolean,
+id_account bigint,
 foreign key (id_account) references account(id),
 foreign key (position_id) references `position`(id)
 );
 create table customer (
-id int  primary key auto_increment,
+id bigint  primary key auto_increment,
 name varchar(45),
 day_of_birth varchar(45),
-phone_number varchar(45),
-id_card varchar(45) unicode,
+phone_number varchar(45) unicode,
 address varchar (225),
 gender boolean,
 status boolean,
-is_deleted boolean,
+health_condition varchar(255),
 email varchar(45) unique,
-health_condition varchar(255)
+id_account bigint,
+foreign key (id_account) references account(id)
 );
-create table type(
+create table product_type(
 id int primary key auto_increment, 
 name varchar (45)
 ) ;
 create table product (
-id int  primary key auto_increment,
-image varchar(500),
+id bigint  primary key auto_increment,
 name varchar(45),
+img varchar(255),
 prices double,
+is_deleted boolean,
 description varchar(500),
 date_submitted date,
 quantity int ,
 type_product int,
-employee_id int ,
+employee_id bigint ,
 foreign key (employee_id) references employee(id),
-foreign key (type_product) references type(id)
+foreign key (type_product) references product_type(id)
 );
+
 create table `order` (
-id int primary key auto_increment ,
+id bigint primary key auto_increment ,
 code_oder varchar(45) unique,
 date_purchase date ,
 total_pay double,
-product_id int ,
-customer_id int ,
-foreign key (product_id) references product(id),
+customer_id bigint ,
 foreign key (customer_id) references customer(id)
 );
- 
+create table order_detail(
+id bigint auto_increment primary key,
+amount int,
+order_id bigint,
+product_id bigint,
+foreign key (product_id) references product(id),
+foreign key (order_id) references `order`(id)
+)
